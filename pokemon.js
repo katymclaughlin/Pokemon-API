9bbe18d98aea5c7f7bc8a41cb6a918347d72c7ab
Katy McLaughlin <mclaughlinkaty6@gmail.com>
2023-12-09 20:47:23 -0600

    "added moves, colors, and habitat columns"

--- pokemon.js
+++ pokemon.js
@@ -18,22 +18,56 @@ fetch("https://pokeapi.co/api/v2/move/")
 
     })
 */
-    fetch ("https://pokeapi.co/api/v2/pokemon-color/")
+
+fetch("https://pokeapi.co/api/v2/move/")
+.then ((data1)=>{
+    //console.log(data1);
+    return data1.json();
+    }) .then(function(json) {
+        let pokemonMoves = json.results;
+        console.log(pokemonMoves);
+        let tableData="";
+        pokemonMoves.map((values)=>{
+            tableData+=`<tr>
+            <td>${values.name}</td>
+          </tr>`;
+        });
+        document.getElementById("table_body").innerHTML=tableData;
+    })
+
+   fetch ("https://pokeapi.co/api/v2/pokemon-color/")
     .then ((data)=>{
        // console.log(data); //json format
         return data.json(); //converted to object
     }) .then(function(json){
     let moves = json.results;
     console.log(moves);
-       let tableData="";
+       let tableData1="";
         moves.map((values)=>{
-            tableData+=`<tr>
+            tableData1+=`<tr>
             <td>${values.name}</td>
-            <td>${values.url}</td>
           </tr>`;
         });
-        document.getElementById("table_body").innerHTML=tableData;
+        document.getElementById("table_body1").innerHTML=tableData1;
     }) .catch((errMessage)=>{
         console.log(errMessage)
     })
-  
+
+
+   fetch ("https://pokeapi.co/api/v2/pokemon-habitat/")
+   .then ((data2)=>{
+      // console.log(data); //json format
+       return data2.json(); //converted to object
+   }) .then(function(json){
+   let habitat = json.results;
+   console.log(habitat);
+      let tableData2="";
+       habitat.map((values)=>{
+           tableData2+=`<tr>
+           <td>${values.name}</td>
+         </tr>`;
+       });
+       document.getElementById("table_body2").innerHTML=tableData2;
+   }) .catch((errMessage)=>{
+       console.log(errMessage)
+   })
